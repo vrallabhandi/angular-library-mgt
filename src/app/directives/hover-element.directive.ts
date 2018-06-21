b59e8bddd8374d1b3bf06ba0391c21e90f4c982b
vrallabhandi <vrallabhandi@allianceglobalservices.com>
2018-06-21 20:31:28 +0530

    custom directive host binding

--- src/app/directives/hover-element.directive.ts
+++ src/app/directives/hover-element.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHoverElement]'
 })
 export class HoverElementDirective {
+  @HostBinding('style.fontWeight') fontWeight: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('mouseenter') mouseEnter() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'yellow');
+    this.fontWeight = 'bold';
   }
 
   @HostListener('mouseleave') mouseLeave() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'transparent');
+    this.fontWeight = 'normal';
   }
 
 }
