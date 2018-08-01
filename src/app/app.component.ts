c3538b4c9f44c4dd4d3101b30b0515260e7f79b0
Venkateshwar Nilagiri <Venkateshwar_Nilagiri@epam.com>
2018-08-01 14:39:27 +0530

    Assignment changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,6 @@ import { Book } from './models/book.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //title = 'world of Angular';
-  book1: Book =
-  {
-    code: "B001",
-    author: "AAAA",
-    name: "C++",
-    thumbnail: "http://www.vikaspublishing.com/uploads/bookimages/vikas-books/9789325975644.jpg"
-  } 
-
-  book2: Book =
-  {
-    code: "B002",
-    author: "Data Structures Using C",
-    name: "C++",
-    thumbnail: "http://www.vikaspublishing.com/uploads/bookimages/vikas-books/9789325975644.jpg"
-  } 
+  
+  
 }
