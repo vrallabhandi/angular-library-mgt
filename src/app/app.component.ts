e78db6de4bf18d393127c4199d607f64557f9d27
Venkateshwar Nilagiri <Venkateshwar_Nilagiri@epam.com>
2018-08-09 18:17:29 +0530

    Pending changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Book } from './models/book.model';
-import { Country } from './models/country.model';
-import { State } from './models/state.model';
-import { City } from './models/city.model';
 import { BooksService } from './services/books.service';
 
 
@@ -15,70 +12,26 @@ export class AppComponent implements OnInit {
   @ViewChild('bName') bookName: ElementRef;
   name: string = 'default value'
   selectedBook: Book;
-  selectedCountry: Country;
-  selectedState: State;
-  selectedCity: City;
   books: any;
 
-  constructor(private booksService: BooksService) {}
+  constructor(private booksService: BooksService) { }
 
   ngOnInit() {
     this.booksService.getBooks()
-    .subscribe(
-      (books: Book[]) => {
-        this.books = books;
-      }, (err: any) => {
-        console.log(err);
-      })
-      };   
-    
-  
-  countries: Country[] = [
-    { name: "USA" },
-    { name: "India" }    
-  ];
+      .subscribe(
+        (books: Book[]) => {
+          this.books = books;
+        }, (err: any) => {
+          console.log(err);
+        })
+  };
 
-  states: State[] = [
-    { name: "California", countrtName: "USA" },
-    { name: "Florida", countrtName: "USA" },
-    { name: "Telangana", countrtName: "India" },
-    { name: "Andhra Pradesh", countrtName: "India"}
-  ];
-
-  cities: City[] = [
-    { name: "Los Angeles", statetName: "California"},
-    { name: "San Francisco", statetName: "California"},
-    { name: "Miami", statetName: "Florida"},
-    { name: "Tampa", statetName: "Florida"},
-    { name: "Hyderabad", statetName: "Telangana"},
-    { name: "Secundrabad", statetName: "Telangana"},
-    { name: "Vijayawada", statetName: "Andhra Pradesh"},
-    { name: "Vizag", statetName: "Andhra Pradesh"}
-  ]
-
- 
-
-  OnBookSelected(data: Book) {    
+  OnBookSelected(data: Book) {
     this.selectedBook = data;
     console.log(this.bookName.nativeElement.value);
   }
 
   checkName(element: HTMLInputElement) {
     console.log(element.value);
-  }
-
-  OnCountrySelected(data: Country) {
-    this.selectedCountry = data;
-    console.log(data.name);
-  }
-
-  OnStateSelected(data: State) {
-    this.selectedState = data;
-    console.log(data.name);
-  }
-
-  OnCitySelected(data: City) {
-    this.selectedCity = data;
-    console.log(data.name);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
-import { CountryComponent } from './country/country.component';
-import { StateComponent } from './state/state.component';
-import { CityComponent } from './city/city.component';
 import { SelectedBookComponent } from './selected-book/selected-book.component';
 import { HighlighterDirective } from './directives/highlighter/highlighter.directive';
 import { HoverElementDirective } from './directives/hover-element/hover-element.directive';
@@ -17,9 +14,6 @@ import { BooksService } from './services/books.service';
   declarations: [
     AppComponent,
     BookComponent,
-    CountryComponent,
-    StateComponent,
-    CityComponent,
     SelectedBookComponent,
     HighlighterDirective,
     HoverElementDirective
