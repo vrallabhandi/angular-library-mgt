331bbcce41b672c41d6b417edf78a8bec1febb7b
Venkateshwar Nilagiri <Venkateshwar_Nilagiri@epam.com>
2018-08-13 14:39:18 +0530

    Pending changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 import { SelectedBookComponent } from './selected-book/selected-book.component';
@@ -20,7 +20,8 @@ import { BooksService } from './services/books.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [BooksService],
   bootstrap: [AppComponent]
