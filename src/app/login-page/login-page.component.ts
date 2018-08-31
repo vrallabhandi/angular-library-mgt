8ac31d548660b469c93dcc542f638923750ef9b7
vrallabhandi <vrallabhandi@allianceglobalservices.com>
2018-08-31 20:40:33 +0530

    Added the login form with built in validations

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('loginForm') loginForm: NgForm;
+  loginFailed = false;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    const model = this.loginForm.value;
+    this.loginFailed = false;
+    if (model.uname === 'test' && model.pwd === 'test') {
+      this.router.navigate(['/home/1']);
+    } else {
+      this.loginFailed = true;
+    }
   }
 
 }
