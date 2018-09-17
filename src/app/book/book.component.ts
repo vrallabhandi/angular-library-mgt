f75081143833dd198d61828a305f09976fe89af4
Prabhu Kollapalli <Prabhu_Kollapalli@epam.com>
2018-09-17 15:41:56 +0530

    assignment

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Book } from '../models/book.model';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
+
+  @Input() book: Book;
+  @Output() selected: EventEmitter<Book> = new EventEmitter<Book>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+bookClick(){
+  //console.log(data);
+  this.selected.emit(this.book);
+}
+
+}
